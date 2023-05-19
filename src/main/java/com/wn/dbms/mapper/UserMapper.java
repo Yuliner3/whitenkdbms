b6cb5eb34ec5c843bfb8ec6191de24982d3f56fc
Yuliner3 <50834315+Yuliner3@users.noreply.github.com>
2023-05-19 15:41:07 +0800

    Delete src directory

--- src/main/java/com/wn/dbms/mapper/UserMapper.java
+++
@@ -1,9 +0,0 @@
-package com.wn.dbms.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.wn.dbms.entity.User;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface UserMapper extends BaseMapper<User> {
-}
